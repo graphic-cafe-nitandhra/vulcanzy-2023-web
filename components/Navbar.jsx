31baba3a9edab736ce0e0f0753d7a07f2a291f26
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2023-02-16 00:08:59 +0530

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,3 @@
 export const Navbar = ({}) => {
-  return <header className="w-full bg-black text-white">Hello</header>;
+  return <header className="w-full bg-black text-white">Hello World 2023</header>;
 };
