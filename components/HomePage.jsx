1dae72e9f04f3c134e1c9e81a77639a201bd4eeb
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2023-02-17 23:27:44 +0530

    Updated home page layout and hero page cover.

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -3,30 +3,37 @@ import Timer from './Timer';
 import vul_black from '../public/assets/imgs/Vulcanzy_black.png';
 function HomePage() {
   return (
-    <div className="flex pt-[140px] flex-col items-center justify-start h-full">
-      <Image src={vul_black} alt="Black-Logo" width={250} height={100}></Image>
-      <h1 className="subhead text-center text-white">
-        <i>#RHAPSODY</i>
-      </h1>
-      {/* <h1 className="text-[3rem] text-white">LET THE MUSIC BEGIN!</h1>
-      <h2 className="text-[3rem] my-16 text-white">
-        <Timer />
-      </h2> */}
-      <div className="text-white my-[18rem] font-[400]  flex items-center  justify-between w-full">
-        <div className="ml-12">
-          <h2 className="text-[2rem]">Join the Rhythm of #RHAPSODY:</h2>
-          <h2 className="text-[1.5rem]">
-            Experience the ultimate college fest celebration
-          </h2>
+    <div>
+      <div className="flex pt-[140px] flex-col items-center justify-start h-screen">
+        <Image
+          src={vul_black}
+          alt="Black-Logo"
+          width={250}
+          height={100}
+        ></Image>
+        <h1 className="subhead text-center text-white">
+          <i>#RHAPSODY</i>
+        </h1>
+        <div className="text-white mt-[18rem] font-[700]  flex items-center  justify-between w-full">
+          <div className="ml-12">
+            <h2 className="text-[2rem]">JOIN THE RYTHM OF #RHAPSODY:</h2>
+            <h2 className="text-[1.5rem] font-[700]">
+              Experience the ultimate college fest celebration
+            </h2>
+          </div>
+          <label className=" text-[1.5rem] mr-12 flex items-center justify-center w-[100px] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]">
+            Join
+          </label>
         </div>
-        <label className=" text-[1.5rem] mr-12 flex items-center justify-center w-[100px] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]">
-          Join
-        </label>
       </div>
-      <h1 className="text-[3rem] text-black">LET THE MUSIC BEGIN!</h1>
-      <h2 className="text-[3rem] my-16 text-black">
-        <Timer />
-      </h2>
+      <div className=" flex flex-col items-center justify-center h-screen bg-black text-white">
+        <h1 className="text-[3rem] text-center text-white">
+          LET THE MUSIC BEGIN!
+        </h1>
+        <h2 className="text-[3rem] my-16 text-white">
+          <Timer />
+        </h2>
+      </div>
     </div>
   );
 }
