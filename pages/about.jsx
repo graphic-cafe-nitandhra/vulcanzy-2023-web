3519ba5a76e744d43fc696c27dfbda01c05423fb
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2023-02-18 01:12:07 +0530

    About us page done.

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,8 +3,21 @@ import Default from '@/layouts/default';
 function About() {
   return (
     <Default>
-      <div className="mt-[120px]">
-        <h1>About Us coming soon!</h1>
+      <div className="mt-[120px] text-white h-screen  bg-black">
+        <div className="mt-[12.5rem] flex flex-col  items-center">
+          <h1 className="text-[1.75rem] mr-12 flex items-center justify-center w-[10rem] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]">
+            About Us
+          </h1>
+          <h2 className="w-[60rem] mt-[3rem] text-[1.5rem] text-center">
+            The fact that NIT Andhra Pradesh is a melting pot of cultural and
+            technical events is part of what makes it so appealing. Vulcanzy, a
+            three-day Techno-Cultural festival, aims to enlighten technical
+            passion while also instilling cultural zeal.Taking in the beauty of
+            a series of fun-filled occurrences, amusing them, and reliving them
+            Join the rhythm of #Rhapsody to experience the ultimate college fest
+            celebration.
+          </h2>
+        </div>
       </div>
     </Default>
   );
