8251026a65ebc4fd1f436465f34bf7c478d01fde
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2023-02-18 15:32:33 +0530

    Basic functionality of Events done

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,10 +1,73 @@
 import Default from '@/layouts/default';
-
+import { useState } from 'react';
 function Events() {
+  let nums = [
+    {
+      val: 1,
+      cat: 'odd',
+    },
+    {
+      val: 2,
+      cat: 'even',
+    },
+    {
+      val: 3,
+      cat: 'odd',
+    },
+    {
+      val: 4,
+      cat: 'even',
+    },
+    {
+      val: 5,
+      cat: 'odd',
+    },
+    {
+      val: 6,
+      cat: 'even',
+    },
+    {
+      val: 7,
+      cat: 'odd',
+    },
+  ];
+  const [events, setEvents] = useState(nums);
+  function clickHandler(catg) {
+    const upd_list = nums.filter(each => {
+      return each.cat === catg;
+    });
+    setEvents(upd_list);
+  }
   return (
     <Default>
-      <div className="mt-[120px]">
-        <h1>All events</h1>
+      <div className="mt-[120px] text-white">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex justify-between">
+            <h1
+              onClick={() => clickHandler('odd')}
+              className="text-[1.75rem] flex mx-8 cursor-pointer items-center justify-center w-[10rem] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]"
+            >
+              All
+            </h1>
+            <h1
+              onClick={() => clickHandler('even')}
+              className="text-[1.75rem] flex mx-8 cursor-pointer items-center justify-center w-[10rem] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]"
+            >
+              Technical
+            </h1>
+            <h1
+              onClick={() => setEvents(nums)}
+              className="text-[1.75rem] flex mx-8 cursor-pointer items-center justify-center w-[10rem] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]"
+            >
+              Cultural
+            </h1>
+          </div>
+          <div className="flex flex-wrap">
+            {events.map(e => {
+              return <h4>{e.val}</h4>;
+            })}
+          </div>
+        </div>
       </div>
     </Default>
   );
--- pages/sponsors.jsx
+++ pages/sponsors.jsx
@@ -3,8 +3,12 @@ import Default from '@/layouts/default';
 function Sponsors() {
   return (
     <Default>
-      <div className="mt-[120px]">
-        <h1>Sponsors coming soon!</h1>
+      <div className="mt-[120px] text-white">
+        <div className="flex flex-col items-center">
+          <h1 className="text-[1.75rem] flex items-center justify-center w-[10rem] h-[50px] rounded-[50px]  border-spacing-16 bg-[#DC4B5D]">
+            Sponsors
+          </h1>
+        </div>
       </div>
     </Default>
   );
