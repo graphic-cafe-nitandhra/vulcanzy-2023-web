39a350d526b0910d0a1c7de2fb8b3b89d0269959
MAHAMMED NAYEEM AKBARSAGARI <85838699+asmdnayeeem@users.noreply.github.com>
2023-02-19 16:29:56 +0530

    responsive navbar added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,23 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import { Spin as Hamburger } from 'hamburger-react'
 import Link from 'next/link';
 import vul_logo from '../public/assets/imgs/Vulcanzy_logo.png';
 export const Navbar = ({}) => {
+  const [isOpen, setOpen] = useState(false)
+  const expand = () => {
+    if (isOpen) {
+      document.querySelector('.menu').classList.add('max-[920px]:hidden')
+    } else {
+      document.querySelector('.menu ').classList.remove('max-[920px]:hidden')
+    }
+  }
+
   return (
     <div className="home">
-      <div className="nav w-full  bg-black fixed top-0 left-0 text-white text-2xl z-10">
-        <ul className="flex flex-wrap justify-between items-center">
-          <li className="flex-column text-center ml-10">
+      <div className="nav w-full  bg-black fixed top-0 left-0 text-white text-2xl z-10 ">
+        <ul className="flex flex-wrap justify-between items-center max-[920px]:flex-col max-[920px]:items-start">
+          <li className="flex-column text-center ml-10 ">
             <Link href="/">
               <Image
                 className="mt-5"
@@ -18,7 +29,7 @@ export const Navbar = ({}) => {
               <label>Vulcanzy</label>
             </Link>
           </li>
-          <div className="flex flex-wrap w-8/12 justify-between  ">
+          <div className= "menu flex flex-wrap  w-8/12 mr-10 justify-between max-[920px]:flex-col max-[920px]:hidden gap-6 duration-600" >
             <li>
               <Link href="/">Home</Link>
             </li>
@@ -31,12 +42,16 @@ export const Navbar = ({}) => {
             <li>
               <Link href="/gallery">Gallery</Link>
             </li>
-            <li className="mr-10">
+            <li className="">
               <Link href="/about">About Us</Link>
             </li>
           </div>
+          <li className="absolute top-8 right-5 min-[920px]:hidden"><Hamburger toggled={isOpen} toggle={setOpen} onToggle={expand}/></li>
+
         </ul>
+
       </div>
+
     </div>
   );
 };
